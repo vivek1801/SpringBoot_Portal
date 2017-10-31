30b8034942a3045b3fa2bd5a0e0788037b04ef84
vivek1801 <33251572+vivek1801@users.noreply.github.com>
2017-10-31 15:51:50 +0530

    Create EmployeeBean.java

---
+++ src/main/java/com/mypersonal/demo/employee/EmployeeBean.java
@@ -0,0 +1,79 @@
+package com.mypersonal.demo.employee;
+
+public class EmployeeBean {
+	private String empId;
+	private String empName;
+	private String empDept;
+	private String mobileNumber;
+
+	EmployeeBean() {
+
+	}
+
+	EmployeeBean(String empId,String empName,String empDept,String mobileNumber) {
+		this.empId=empId;
+		this.empName=empName;
+		this.empDept=empDept;
+		this.mobileNumber=mobileNumber;
+	}
+
+	/**
+	 * @return the empId
+	 */
+	public String getEmpId() {
+		return empId;
+	}
+
+	/**
+	 * @param empId
+	 *            the empId to set
+	 */
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	/**
+	 * @return the empName
+	 */
+	public String getEmpName() {
+		return empName;
+	}
+
+	/**
+	 * @param empName
+	 *            the empName to set
+	 */
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	/**
+	 * @return the empDept
+	 */
+	public String getEmpDept() {
+		return empDept;
+	}
+
+	/**
+	 * @param empDept
+	 *            the empDept to set
+	 */
+	public void setEmpDept(String empDept) {
+		this.empDept = empDept;
+	}
+
+	/**
+	 * @return the mobileNumber
+	 */
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	/**
+	 * @param mobileNumber
+	 *            the mobileNumber to set
+	 */
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+}
